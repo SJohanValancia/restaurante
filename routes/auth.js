a977477bf4b54581e6ff00db8da0efc5789922d3
Johan Steven Valencia Mejias <johanvalencia@MacBook-Air-de-Johan.local>
2025-10-03 11:49:00 -0500

    registro y login 4

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user'); // Asegúrate que la ruta sea correcta
+const User = require('../models/User'); // Asegúrate que la ruta sea correcta
 const jwt = require('jsonwebtoken');
 
 // Generar JWT Token
