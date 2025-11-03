519540083be0b71a873ae2f783236ca800b69a49
Johan Steven Valencia Mejias <johanvalencia@MacBook-Air-de-Johan.local>
2025-11-03 12:25:15 -0500

    nuevo alimentos 3

--- routes/alimentos.js
+++ routes/alimentos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Alimento = require('../models/Alimento');
+const Alimento = require('../models/alimento');
 const Product = require('../models/Product');
 const { protect } = require('../middleware/auth');
 
