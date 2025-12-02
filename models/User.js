af5c11d7424dfb9f6564ff17230bfe46437086f8
Johan Valencia <johanmejiaxlarchaval@gmail.com>
2025-12-02 15:27:56 -0500

    bloqueo cuenta 2

--- models/User.js
+++ models/User.js
@@ -39,7 +39,7 @@ const userSchema = new mongoose.Schema({
   },
   rol: {
     type: String,
-    enum: ['admin', 'mesero', 'cajero'],
+    enum: ['admin', 'mesero', 'cajero', 'superadmin'],
     default: 'mesero'
   },
   activo: {
